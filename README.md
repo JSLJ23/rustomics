17c5524eec824e44e692b35704dc6ab732ee4944
Joshua Soon <joshuaslj231094@gmail.com>
2023-01-18 22:23:56 +0800

    feat: hashmap for chromosome name mapping and write output file.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Rustomics
+
+## Tools for bioinformatics built in awesome Rust!
+
+## Currentlt available:
+- BED file chromosome renaming tool.
+
+## Build instructions:
+- Once Rust and Cargo have been installed and this repo has been cloned, run:
+    ```rust
+    cargo build --release
+    ```
+- The binary will be found in `target/release.rustomics`.
+- Run `rustomics --help` for guide on usage.
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use clap::{arg, Command, ArgMatches};
 use std::path::Path;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
+use std::collections::HashMap;
+use std::fs;
+
 
 fn main() {
     let matches: ArgMatches = Command::new("MyApp")
         .version("0.1")
         .author("Joshua Soon")
-        .about("Does awesome things")
+        .about("Tools for bioinformatics built in awesome Rust!")
         .arg(arg!(--bedfile <VALUE>).required(true))
         .arg(arg!(--mapfile <VALUE>).required(true))
         .get_matches();
@@ -30,9 +33,29 @@ fn main() {
         std::process::exit(1)
     }
 
-    let bed_file = File::open(bed_file_path).unwrap();
-    let bed_reader = BufReader::new(bed_file);
-    for bed_lines in bed_reader.lines() {
-        println!("{}", bed_lines.unwrap());
+    let mut chromosome_map : HashMap<String, String>= HashMap::new();
+
+    let map_file: File = File::open(map_file_path).unwrap();
+    let map_reader: BufReader<File> = BufReader::new(map_file);
+    for map_line in map_reader.lines(){
+        let map_lines_split:Vec<String> = Vec::from_iter(map_line.unwrap().split("\t").map(String::from));
+        chromosome_map.insert(map_lines_split[0].to_string(), map_lines_split[1].to_string());
+        println!("{},{}", map_lines_split[0].to_string(), map_lines_split[1].to_string());
     }
+
+    let mut new_bed_file_vec: Vec<String> = Vec::new();
+
+    let bed_file: File = File::open(bed_file_path).unwrap();
+    let bed_reader: BufReader<File> = BufReader::new(bed_file);
+    for bed_lines in bed_reader.lines() {
+        let bed_line: String = bed_lines.unwrap();
+        let chromosome_identifer: &str = bed_line.split_whitespace().next().unwrap();
+        let new_chromosome_identifier: &str = chromosome_map.get(chromosome_identifer).unwrap().as_str();
+        let new_bed_line: String = bed_line.replace(chromosome_identifer, new_chromosome_identifier);
+        new_bed_file_vec.push(new_bed_line);
+    }
+
+    let output_bed_file = format!("{}{}", "renamed_", bed_file_name);
+    fs::write(output_bed_file, new_bed_file_vec.join("\n")).expect("");
+
 }
